990ddcfaa876759d764dacbbac4380b1b63a9d7f
kavyaskillup-sko <kavya.s@skillup.online>
2026-08-19 13:54:40 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -12,7 +12,7 @@ terraform {
     resource_group_name  = "sko-tfstate-rg"
     storage_account_name = "skotfstateaccount"
     container_name       = "tfstate"
-    key                  = "vm-${terraform.workspace}.tfstate"
+    key                  = "vm-dev.tfstate"
   }
 }
 
