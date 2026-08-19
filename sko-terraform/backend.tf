ab90962696e12ac85372b04c3b712553330a577a
kavyaskillup-sko <kavya.s@skillup.online>
2026-08-19 12:38:07 +0530

    Delete sko-terraform directory

--- sko-terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "sko-tfstate-rg"
-    storage_account_name = "skotfstateaccount"
-    container_name       = "tfstate"
-    key                  = "vm-${terraform.workspace}.tfstate"
-  }
-}
