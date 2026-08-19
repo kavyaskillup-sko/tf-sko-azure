653edb00904fe4da96bdc4211ef7856c9c65fd40
kavyaskillup-sko <kavya.s@skillup.online>
2026-08-19 13:47:45 +0530

    Configure Terraform backend for Azure storage

--- versions.tf
+++ versions.tf
@@ -1,12 +1,21 @@
 terraform {
   required_version = "= 1.12.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "sko-tfstate-rg"
+    storage_account_name = "skotfstateaccount"
+    container_name       = "tfstate"
+    key                  = "vm-${terraform.workspace}.tfstate"
+  }
 }
+
 provider "azurerm" {
   features {}
 }
