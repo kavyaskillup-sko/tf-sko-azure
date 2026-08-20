2ba80291ab1fb53b2cc3513a7643fa8914539b36
kavyaskillup-sko <kavya.s@skillup.online>
2026-08-20 13:05:06 +0530

    Refactor VM variables in variables.tf
    Removed unnecessary variables and added new ones for VM configuration.

--- modules/vm/variables.tf
+++ modules/vm/variables.tf
@@ -1,24 +1,21 @@
-variable "env" {
-  description = "Environment name (dev, prod, etc.)"
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "Name of the resource group where VM will be created"
-  type        = string
-}
-
 variable "location" {
-  description = "Azure region for resources"
-  type        = string
+  type = string
 }
-
-variable "nic_id" {
-  description = "ID of the network interface to attach to the VM"
-  type        = string
+variable "environment_tag" {
+  type = string
 }
-
+variable "vm_size" {
+  type = string
+}
+variable "subnet_id" {
+  type = string
+}
+variable "resource_group" {
+  type = string
+}
+variable "admin_username" {
+  type = string
+}
 variable "ssh_public_key" {
-  description = "Path to the SSH public key file"
-  type        = string
+  type = string
 }
