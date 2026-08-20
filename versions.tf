6b79084aa11afafdcab36e18280929455f94675b
kavyaskillup-sko <kavya.s@skillup.online>
2026-08-20 12:15:34 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -8,12 +8,7 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "sko-tfstate-rg"
-    storage_account_name = "skotfstateaccount"
-    container_name       = "tfstate"
-    key                  = "vm-dev.tfstate"
-  }
+  backend "azurerm" {}
 }
 
 provider "azurerm" {
