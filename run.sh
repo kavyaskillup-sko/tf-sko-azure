58c667c6d8b09096f27208b89bd33531546dbdb9
kavyaskillup-sko <kavya.s@skillup.online>
2026-08-20 13:10:02 +0530

    Commented sh file

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
-set -e
+# #!/bin/bash
+# set -e
 
-ENV=$1
-if [ -z "$ENV" ]; then
-  echo "Usage: ./run.sh <dev|prod>"
-  exit 1
-fi
+# ENV=$1
+# if [ -z "$ENV" ]; then
+#   echo "Usage: ./run.sh <dev|prod>"
+#   exit 1
+# fi
 
-cd $ENV
-terraform workspace select $ENV || terraform workspace new $ENV
-terraform init -backend-config=../backend.tf
-terraform validate
-terraform plan -var-file=$ENV.tfvars -out=tfplan
-terraform apply -auto-approve tfplan
+# cd $ENV
+# terraform workspace select $ENV || terraform workspace new $ENV
+# terraform init -backend-config=../backend.tf
+# terraform validate
+# terraform plan -var-file=$ENV.tfvars -out=tfplan
+# terraform apply -auto-approve tfplan
