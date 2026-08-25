67e1a471b8535d9b60475dc835c8c430bd6a6a4c
kavyaskillup-sko <kavya.s@skillup.online>
2026-08-25 12:40:12 +0530

    Add descriptions to VM outputs and rename private IP output

--- modules/vm/outputs.tf
+++ modules/vm/outputs.tf
@@ -1,7 +1,10 @@
+# Outputs for use in other modules or root
 output "vm_name" {
-  value = azurerm_linux_virtual_machine.vm.name
+  description = "Name of the created VM"
+  value       = azurerm_linux_virtual_machine.vm.name
 }
 
-output "private_ip" {
-  value = azurerm_network_interface.nic.ip_configuration[0].private_ip_address
+output "vm_private_ip" {
+  description = "Private IP address of the VM"
+  value       = azurerm_network_interface.nic.ip_configuration[0].private_ip_address
 }
